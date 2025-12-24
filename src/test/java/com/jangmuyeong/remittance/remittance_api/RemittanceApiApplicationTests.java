d18707cb678abda53ded2166399a86a746a3050f
wkdan <jmy8876@gmail.com>
2025-12-24 22:50:18 +0900

    Initial commit

---
+++ src/test/java/com/jangmuyeong/remittance/remittance_api/RemittanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jangmuyeong.remittance.remittance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemittanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
