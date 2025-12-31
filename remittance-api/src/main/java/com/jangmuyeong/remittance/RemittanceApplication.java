29d6a3a33507203e8801dfcd54e9df8b66bd75c9
wkdan <jmy8876@gmail.com>
2025-12-31 21:27:24 +0900

    feat: Remittance 애플리케이션 엔트리 포인트 추가

---
+++ remittance-api/src/main/java/com/jangmuyeong/remittance/RemittanceApplication.java
@@ -0,0 +1,14 @@
+package com.jangmuyeong.remittance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 애플리케이션 엔트리 포인트
+ */
+@SpringBootApplication
+public class RemittanceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RemittanceApplication.class, args);
+	}
+}
